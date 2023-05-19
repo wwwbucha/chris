76a661e959137a9d95f80e3ce6616dff678f25ad
wwwbucha <christian03enzo0419@gmail.com>
2023-05-19 15:21:31 -0300

    Create teste_while.py

---
+++ teste_while.py
@@ -0,0 +1,7 @@
+while True: 
+    valor =int(input("DIGITE O VALOR 1 OU 0 PARA ENCERRAR:  "))
+    if valor==1:
+        print("valor correto")
+    else:
+        print("valor pra sair ")
+        break
